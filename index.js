8f6f432953322612c0b3c8fd2fbf075ce0f9576e
Bryan <@BCabrera2021302>
2023-01-18 14:53:01 -0600

    version 1 commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+//const valor1 = document.getElementById('valor1');
+//const valor2 = document.getElementById('valor2');
+
+
+
+function suma(){
+let valor1 = document.getElementById("valor1").value;
+let valor2 = document.getElementById("valor2").value;
+let suma = parseInt(valor1) + parseInt(valor2);
+console.log(suma);
+}
+
+
+
